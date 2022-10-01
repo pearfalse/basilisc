2aca2f50cd3369958267a76097d041209e7c8e52
Simon Harris <simon@pearfalse.com>
2022-10-01 22:58:08 +0100

    Add partial/full slice iteration for SubArray

--- meta-src/subarray.rs
+++ meta-src/subarray.rs
@@ -1,14 +1,16 @@
+use std::{ops::{Index, IndexMut}, num::NonZeroUsize};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub(crate) struct SubArray<'a, T> {
+pub struct SubArray<'a, T> {
 	slice: &'a [T],
 	from: usize,
 }
 
 impl<'a, T> SubArray<'a, T> {
-	pub fn new(slice: &'a [T], from: usize) -> Self {
+	pub const fn new(slice: &'a [T], from: usize) -> Self {
 		// ensure top of slice is in range
 		if from.checked_add(slice.len().saturating_sub(1)).is_none() {
-			panic!("slice of length {} is too big for start offset {}", slice.len(), from);
+			panic!("slice is too big for given start offset");
 		}
 
 		SubArray { slice, from }
@@ -17,7 +19,54 @@ impl<'a, T> SubArray<'a, T> {
 	pub fn get(&self, index: usize) -> Option<&'a T> {
 		index.checked_sub(self.from).and_then(|raw_idx| self.slice.get(raw_idx))
 	}
+
+	pub fn raw_slice(&self) -> &'a [T] {
+		self.slice
+	}
 }
+
+impl<'a, T: Default + Copy> SubArray<'a, T> {
+	pub fn full_iter(&self) -> FullIter<'a, T> {
+		FullIter::new(self)
+	}
+}
+
+impl<'a, T> Index<usize> for SubArray<'a, T> {
+	type Output = T;
+
+	fn index(&self, index: usize) -> &Self::Output {
+		self.get(index).expect("index out of range")
+	}
+}
+
+pub struct FullIter<'a, T> {
+	slice: core::slice::Iter<'a, T>,
+	skip_count: Option<NonZeroUsize>,
+}
+
+impl<'a, T: Default + Copy> FullIter<'a, T> {
+	fn new(src: &SubArray<'a, T>) -> Self {
+		Self {
+			slice: src.slice.iter(),
+			skip_count: NonZeroUsize::new(src.from),
+		}
+	}
+}
+
+impl<'a, T: Default + Copy> Iterator for FullIter<'a, T> {
+	type Item = T;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		if let Some(old) = self.skip_count {
+			// don't return anything
+			self.skip_count = NonZeroUsize::new(old.get() - 1);
+			Some(T::default())
+		} else {
+			self.slice.next().copied()
+		}
+	}
+}
+
 
 #[cfg(test)]
 mod test {
@@ -50,5 +99,15 @@ mod test {
 	fn critical() {
 		let _ = SubArray::new(&[0, 0], usize::MAX - 1);
 		let _ = SubArray::new(<&'static [u8]>::default(), usize::MAX);
+	}
+
+	#[test]
+	fn full_iter() {
+		let sut = SubArray::new(&[1, 2, 3], 2);
+		let result: Vec<_> = sut.full_iter().collect();
+		assert_eq!(&[0, 0, 1, 2, 3], &*result);
+
+		let sut = SubArray::new(&[5], 0);
+		assert_eq!(Some(5), sut.full_iter().next());
 	}
 }
