e4f47b18def4b3c8d8c1cb2ce5be9206c42e1a98
Simon Harris <simon@pearfalse.com>
2022-11-15 19:07:58 +0000

    Drop removed elements in ArrayVecExt::remove_first

--- src/support/arrayvec_ext.rs
+++ src/support/arrayvec_ext.rs
@@ -1,4 +1,4 @@
-use std::{mem::ManuallyDrop, ptr};
+use std::{mem::{ManuallyDrop, self, MaybeUninit}, ptr};
 
 use arrayvec::ArrayVec;
 
@@ -7,11 +7,12 @@ pub(crate) trait ArrayVecExt<T> {
 	/// position.
 	fn pop_front(&mut self) -> Option<T>;
 
-	/// Removes the first `x` elements from the front of the ArrayVec. The elements are not dropped.
+	/// Removes the first `x` items from the front of the ArrayVec.
 	///
 	/// # Panics
 	///
-	/// Panics if `x` exceeds the ArrayVec's length.
+	/// Panics if `x` exceeds the ArrayVec's length, or if any removed item's `Drop` implementation
+	/// panics. If it does, other removed item may leak.
 	fn remove_first(&mut self, x: usize);
 }
 
@@ -24,7 +25,7 @@ impl<T, const N: usize> ArrayVecExt<T> for ArrayVec<T, N> {
 			ptr::read(self.first()? as *const T)
 		});
 
-		// reduce the length by 1, then move all elements up one place
+		// reduce the length by 1, then move all items up one place
 		unsafe {
 			self.set_len(new_len);
 			ptr::copy(self.as_ptr().add(1), self.as_mut_ptr(), new_len);
@@ -34,14 +35,38 @@ impl<T, const N: usize> ArrayVecExt<T> for ArrayVec<T, N> {
 	}
 
 	fn remove_first(&mut self, x: usize) {
+		let must_drop = mem::needs_drop::<T>() && N > 0;
+
 		let old_len = self.len();
 		let new_len = old_len.checked_sub(x as usize)
-			.unwrap_or_else(|| panic!("couldn't remove {} elements from a {}-wide ArrayVec",
+			.unwrap_or_else(|| panic!("couldn't remove {} items from a {}-wide ArrayVec",
 				x, old_len));
+
+		let mut drop_sites = unsafe {
+			// this is MaybeUninit::uninit_array, but usable on stable
+			MaybeUninit::<[MaybeUninit<T>; N]>::uninit().assume_init()
+		};
+		if must_drop {
+			// copy items to here for dropping later
+			unsafe {
+				ptr::copy(self.as_ptr(), drop_sites[0].as_mut_ptr(), x);
+			}
+		}
+
 		unsafe {
 			let data = self.as_mut_ptr();
 			std::ptr::copy(data.add(x), data, new_len);
 			self.set_len(new_len);
+		}
+
+		if must_drop {
+			// drop removed items
+			for site in &mut drop_sites[..x] {
+				unsafe {
+					// SAFETY: was init'd if MUST_DROP, and if that's false, we don't even run this
+					ptr::drop_in_place(site.as_mut_ptr());
+				}
+			}
 		}
 	}
 
@@ -51,6 +76,64 @@ impl<T, const N: usize> ArrayVecExt<T> for ArrayVec<T, N> {
 #[cfg(test)]
 mod test_arrayvec_ext {
 	use super::*;
+
+	use std::cell::Cell;
+
+	#[derive(Debug)]
+	struct CheckedDropSite {
+		slots: Cell<u32>,
+		next: Cell<u32>,
+	}
+
+	impl CheckedDropSite {
+		#[inline]
+		pub fn new() -> Self {
+			Self {
+				slots: Cell::new(0),
+				next: Cell::new(1 << 0),
+			}
+		}
+
+		#[inline]
+		pub fn all_dropped(&self) -> bool { self.slots.get() == u32::MAX }
+
+		#[inline]
+		pub fn drop_mask(&self) -> u32 { self.slots.get() }
+
+		pub fn next(&self) -> Option<CheckedDrop<'_>> {
+			let mask = Some(self.next.get()).filter(|n| *n != 0)?;
+			self.next.set(self.next.get() << 1);
+
+			Some(CheckedDrop {
+				slots: &self.slots,
+				mask,
+			})
+		}
+	}
+
+	#[derive(Debug)]
+	struct CheckedDrop<'a> {
+		slots: &'a Cell<u32>,
+		mask: u32,
+	}
+
+	impl<'a> CheckedDrop<'a> {
+		#[inline]
+		pub fn index(&self) -> u32 { self.mask.trailing_zeros() }
+
+		#[doc(hidden)]
+		fn _mark_dropped(&self) {
+			// assert this one was not already dropped
+			assert_eq!(0, self.slots.get() & self.mask);
+			self.slots.set(self.slots.get() | self.mask);
+		}
+	}
+
+	impl<'a> Drop for CheckedDrop<'a> {
+		fn drop(&mut self) {
+			self._mark_dropped();
+		}
+	}
 
 	#[test]
 	fn pop_front_with_integers() {
@@ -63,53 +146,40 @@ mod test_arrayvec_ext {
 	// asserts that calling drop glue happens correctly
 	#[test]
 	fn pop_front_check_drop() {
-		use std::cell::Cell;
-
-		let check_slots = Cell::new(0u32);
-
-		#[derive(Debug)]
-		struct CheckedDrop<'a> {
-			slots: &'a Cell<u32>,
-			mask: u32,
-		}
-
-		impl<'a> Drop for CheckedDrop<'a> {
-			fn drop(&mut self) {
-				// assert this one was not already dropped
-				assert_eq!(0, self.slots.get() & self.mask);
-				self.slots.set(self.slots.get() | self.mask);
-			}
-		}
+		let checked_drops = CheckedDropSite::new();
 
 		let mut arr = ArrayVec::<CheckedDrop<'_>, 32>::new();
-		for i in 0..32 {
-			arr.push(CheckedDrop {
-				slots: &&check_slots,
-				mask: 1 << i,
-			});
+		while let Some(cd) = checked_drops.next() {
+			arr.push(cd);
 		}
 
-		assert_eq!(0, check_slots.get());
 		for i in 0..32 {
 			let got = arr.pop_front().unwrap();
 			// assert they were pulled off in order
-			assert_eq!(1 << i, got.mask);
+			assert_eq!(i, got.index());
 			// `got` is dropped here
 		}
-		assert_eq!(u32::MAX, check_slots.get());
 
+		assert!(checked_drops.all_dropped());
 		assert!(arr.is_empty()); // should have pulled them all off by now
 	}
 
 	#[test]
 	fn remove_first() {
-		let mut av: ArrayVec<u8, 9> = ArrayVec::new();
+		let drop_checks = CheckedDropSite::new();
+		let mut av: ArrayVec<(CheckedDrop<'_>, u8), 9> = ArrayVec::new();
+
 		for i in 20..29 {
-			av.push(i);
+			av.push((
+				drop_checks.next().unwrap(), i
+			));
 		}
 
 		av.remove_first(3);
-
-		assert_eq!(&[23, 24, 25, 26, 27, 28], &*av);
+		assert_eq!(0b111, drop_checks.drop_mask());
+		assert_eq!(
+			&[23, 24, 25, 26, 27, 28],
+			&*av.into_iter().map(|(_, i)| i).collect::<ArrayVec<u8, 6>>()
+		);
 	}
 }
