b1a746429aa910ae0f578c53b3a5ee19b562e98b
Simon Harris <simon@pearfalse.com>
2023-01-02 18:41:58 +0000

    Support for multi-byte UTF-8 characters, including across read points

--- src/pack/byte_decode.rs
+++ src/pack/byte_decode.rs
@@ -14,13 +14,14 @@ pub(super) struct ByteDecoder<'a> {
 	buf: MaybeUninit<*mut [u8]>, // heap storage
 	drain: *const [u8], // pull off chars
 	src: IoObject<'a>,
+	last_read_pos: u64,
 }
 
 #[derive(Debug, thiserror::Error)]
 pub(super) enum Error {
 	#[error("invalid UTF-8 character at file position {start_pos}")]
 	InvalidUtf8 { start_pos: u64 },
-	#[error("character '{0}' has no RISC OS Latin-1 equivalent")]
+	#[error("character '{}' has no RISC OS Latin-1 equivalent", (.0).escape_unicode())]
 	NoLatin1Char(char),
 	#[error("I/O error: {0:?}")]
 	IoError(#[from] io::Error),
@@ -52,18 +53,21 @@ impl<'a> ByteDecoder<'a> {
 			buf: MaybeUninit::new(std::ptr::slice_from_raw_parts_mut(buf.as_mut_ptr(), capacity)),
 			drain: std::ptr::slice_from_raw_parts(buf.as_ptr(), 0),
 			src,
+			last_read_pos: 0,
 		}
 	}
 
 	pub fn read_next(&mut self) -> Result<Option<u8>, Error> {
 		let ch: char = loop {
-			match self.try_next_char() {
+			match self.try_next_char()? {
 				Some(c) => break c, // found a char
-				None => {},
+				None => {}, // multi-byte char splits at end of buffer, or buffer ends
 			};
 
 			match self.fill_buf()? {
-				ControlFlow::Break(()) => return Ok(None), // file is done
+				ControlFlow::Break(()) if unsafe {&*self.drain}.len() == 0
+					=> return Ok(None), // file is done
+				ControlFlow::Break(()) => return Err(self.utf8_error()),
 				ControlFlow::Continue(()) => continue,
 			}
 		};
@@ -74,30 +78,85 @@ impl<'a> ByteDecoder<'a> {
 	}
 
 
-	fn try_next_char(&mut self) -> Option<char> {
-		None
+	fn utf8_error(&self) -> Error {
+    	Error::InvalidUtf8 { start_pos: self.last_read_pos }
+	}
+
+	fn try_next_char(&mut self) -> Result<Option<char>, Error> {
+		let slice = unsafe { &*self.drain };
+		let lead = match slice.first().copied() {
+			Some(c) => c,
+			None => return Ok(None),
+		};
+
+		let char8_len = match lead {
+			0x00..=0x7f => 1u8, // ASCII
+			0b110_00000..=0b110_11111 => 2,
+			0b1110_0000..=0b1110_1111 => 3,
+			// all Latin-1 conversions from the SMPs will fail, but we need the char for the error
+			0b11110_000..=0b11110_111 => 4,
+			_ => return Err(self.utf8_error())
+		};
+
+		let char8_slice = if char8_len as usize <= slice.len() {
+			&slice[..(char8_len as usize)]
+		} else {
+			// more read data needed
+			return Ok(None);
+		};
+
+		// TODO increase last_read_pos
+		self.drain = &slice[(char8_len as usize)..] as *const [u8];
+		std::str::from_utf8(char8_slice)
+			.map_err(|_| Error::InvalidUtf8 { start_pos: self.last_read_pos })
+			.map(|s| {
+				self.last_read_pos += char8_len as u64;
+				Some(s.chars().next().unwrap())
+			})
 	}
 
 	fn fill_buf(&mut self) -> io::Result<ControlFlow<()>> {
-		debug_assert!(unsafe {&*self.drain}.is_empty());
+		use std::ptr;
 
-		let buf = unsafe {
+		let entire_buf = unsafe {
 			// SAFETY: always init'd, no one else keeps a mut ref around for this
 			self.buf.assume_init_read()
 		};
 
+		let rem_len = unsafe {
+			// SAFETY: impl to mimic feature `slice_ptr_len`
+			(*self.drain).len()
+		};
 
+		let empty_buf = unsafe {
+			if rem_len > 0 {
+				// move undrained elements to the front and offset `entire_buf`
+				ptr::copy(
+					self.drain as *const u8,
+					entire_buf as *mut u8,
+					rem_len);
+				let space_len = (*entire_buf).len() - rem_len;
+				ptr::slice_from_raw_parts_mut(
+					(entire_buf as *mut u8).add(rem_len),
+					space_len)
+			} else {
+				entire_buf
+			}
+		};
+
+
+		// incorporate length of forwarded elements in this level
 		let read_size = {
 			let buf = unsafe {
 				// SAFETY: values are always init'd
-				&mut *buf
+				&mut *empty_buf
 			};
 			let read_size = self.src.read(buf)?;
 			debug_assert!(read_size <= buf.len());
-			read_size
+			read_size + rem_len
 		};
 
-		self.drain = std::ptr::slice_from_raw_parts(buf as *const u8, read_size);
+		self.drain = std::ptr::slice_from_raw_parts(entire_buf as *const u8, read_size);
 
 		Ok(match read_size {
 			0 => ControlFlow::Break(()),
@@ -128,7 +187,23 @@ mod tests {
 	#[test]
 	fn simple() {
 		check(b"one", b"one", None); // ASCII
-		check(b"fianc\xc3\xa9e", b"fianc\xe9e", None); // non-ASCII
+	}
+
+	#[test]
+	fn non_ascii() {
+		check("fianc\u{00e9}e".as_bytes(), b"fianc\xe9e", None); // non-ASCII
+	}
+
+	#[test]
+	fn multi_iter_no_utf8_split() {
+		// split into multiple iterations
+		check(b"MULTIread", b"MULTIread", Some(3));
+	}
+
+	#[test]
+	fn multi_iter_utf8_split() {
+		// 3 bytes a piece
+		check("\u{2401}\u{2402}\u{2403}\u{2404}".as_bytes(), b"\x01\x02\x03\x04", Some(4));
 	}
 
 	fn check(input: &[u8], output: &[u8], capacity: Option<usize>) {
