d6ce82019bce49776f21c105fad4970d7d322fdf
Simon Harris <simon@pearfalse.com>
2024-08-10 18:31:14 +0100

    Correct build scripts

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 cargo.exe run -p token-data-gen
 
-set RUSTFLAGS="--remap-path-prefix %HOMEDRIVE%%HOMEPATH%\.cargo\registry=/cargo-registry --remap-path-prefix %CD%=/src"
+set RUSTFLAGS=--remap-path-prefix %HOMEDRIVE%%HOMEPATH%\.cargo\registry=\cargo-registry --remap-path-prefix %CD%=\basilisc
 cargo.exe build --release
--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cargo run -p token-data-gen
 
 RUSTFLAGS="\
 --remap-path-prefix $HOME/.cargo/registry=/cargo-registry \
---remap-path-prefix $(pwd)=/src \
+--remap-path-prefix $(pwd)=/basilisc \
 " exec cargo build --release
