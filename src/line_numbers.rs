7ac2a7ddaabf39fd20ab522bcf8878290fa88245
Simon Harris <simon@pearfalse.com>
2024-08-10 12:21:14 +0100

    Allow encoding of line number references; more thorough tests for encoding/decoding

--- src/line_numbers.rs
+++ src/line_numbers.rs
@@ -22,6 +22,9 @@
 //! [^1]: <https://web.archive.org/web/20081203145001/www.nelsonit.net/~jon/BBCMicro/2005/06/27/165311.html>,
 //! via <https://xania.org/200711/bbc-basic-line-number-format>
 
+/// The limit of line numbers; all valid line numbers in a `u16` must be strictly less than this.
+pub const LIMIT: u16 = 0xff00;
+
 use thiserror::Error;
 
 /// A line number reference has three bytes of meaningful info, after the `0x8D` header (not
@@ -30,9 +33,32 @@ pub type Encoded = [u8; 3];
 
 #[derive(Debug, Error, Clone, Copy, PartialEq, Eq)]
 pub enum DecodeError {
-	#[error("out of range (decoded value was &{0:x}, but must be < &ff00")]
+	#[error("out of range (decoded value was &{0:x}, but must be less than &ff00")]
 	OutOfRange(u32),
 }
+
+#[derive(Debug, Error, Clone, Copy, PartialEq, Eq)]
+pub enum EncodeError {
+	#[error("out of range (line number must be less than &ff00)")]
+	OutOfRange,
+}
+
+/// Encodes a line number into its equivalent line reference format.
+///
+/// Returns `Err(EncodeError::OutOfRange)` if `line_number` is not below [`LIMIT`].
+pub fn try_encode(line_number: u16) -> Result<Encoded, EncodeError> {
+	if line_number >= LIMIT { return Err(EncodeError::OutOfRange); }
+
+	let mut r = [0b0101_0100u8, 0b0100_0000, 0b0100_0000];
+	let [ref mut a, ref mut b, ref mut c] = r;
+
+	*b ^= ( line_number &             0b11_1111) as u8;
+	*a ^= ((line_number &           0b1100_0000) >> 2) as u8;
+	*c ^= ((line_number &   0b11_1111_0000_0000) >> 8) as u8;
+	*a ^= ((line_number & 0b1100_0000_0000_0000) >> 12) as u8;
+	Ok(r)
+}
+
 
 /// Decodes a byte array into its equivalent numeric format.
 pub fn try_decode(encoded: Encoded) -> Result<u16, DecodeError> {
@@ -69,31 +95,50 @@ pub fn try_decode(encoded: Encoded) -> Result<u16, DecodeError> {
 }
 
 #[cfg(test)]
-mod test_decode {
+mod test {
 	use super::*;
+
+	macro_rules! assert_case {
+		($encoded:literal, $number:literal) => {
+			::assert_hex::assert_eq_hex!(Ok(*$encoded), try_encode($number));
+			::assert_hex::assert_eq_hex!(Ok($number), try_decode(*$encoded));
+		};
+	}
 
 	#[test]
 	fn simple_cases() {
-		let data_cases = [
-			([0x14, 0x00, 0x00], 0),
-			([0x14, 0x0a, 0x00], 10),
-			([0x54, 0x5e, 0x40], 30),
-			([0x04, 0x00, 0x00], 64),
-			([0x28, 0x3f, 0x3e], 0xfeff),
+		assert_case!(b"T@@", 0);
+		assert_case!(b"TJ@", 10);
+		assert_case!(b"D@@", 64);
+		assert_case!(b"h\x7f~", 0xfeff);
+		assert_case!(b"dH@", 200);
+		assert_case!(b"DE@", 69);
+		assert_case!(b"tdA", 420);
+		assert_case!(b"t[B", 667);
+		assert_case!(b"dHG", 1992);
+		assert_case!(b"Typ", 12345);
+		assert_case!(b"DcC", 867);
+		assert_case!(b"t}T", 5309);
+	}
 
-			// same as above, but with some 0x40s OR'd in to remove ASCII control codes
-			([b'T', b'@', b'@'], 0),
-			([b'T', b'J', b'@'], 10),
-			([b'D', b'@', b'@'], 64),
-			([b'(', b'?', b'>'], 0xfeff),
-		];
-		for (data, expected) in data_cases {
-			assert_eq!(Ok(expected), try_decode(data));
+	#[test]
+	fn encode_out_of_range() {
+		for i in LIMIT..(u16::MAX) {
+			assert_eq!(Err(EncodeError::OutOfRange), try_encode(i));
 		}
 	}
 
 	#[test]
-	fn err_out_of_range() {
+	fn y_u_no_54() {
+		// I created this test to try and work out why the encode fn needs to represent the
+		// first byte starting value of 0x54, and the decode fn doesn't. I still have no idea.
+		for ln in 0..LIMIT {
+			assert_eq!(Some(ln), try_encode(ln).ok().and_then(|e| try_decode(e).ok()));
+		}
+	}
+
+	#[test]
+	fn decode_out_of_range() {
 		let data_cases = [
 			// number is greater than 16-bit
 			([1, 0, 0], 0x1_4040),
