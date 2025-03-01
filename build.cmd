5822048fcfc18550eff46bd74fb16fe81dfd86cd
Simon Harris <simon@pearfalse.com>
2025-03-01 18:48:54 +0000

    Add zipping to (non-Windows) build scripts

--- build.cmd
+++ build.cmd
@@ -2,3 +2,4 @@ cargo.exe run -p token-data-gen
 
 set RUSTFLAGS=--remap-path-prefix %HOMEDRIVE%%HOMEPATH%\.cargo\registry=\cargo-registry --remap-path-prefix %CD%=\basilisc
 cargo.exe build --release
+rem TODO: zipping the results
--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ cargo run -p token-data-gen
 RUSTFLAGS="\
 --remap-path-prefix $HOME/.cargo/registry=/cargo-registry \
 --remap-path-prefix $(pwd)=/basilisc \
-" exec cargo build --release
+" cargo build --release
+zip -j target/release/basilisc{.zip,}
